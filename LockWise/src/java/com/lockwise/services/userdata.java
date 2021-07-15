2d7c141cf2ccf9f8cadc5c7fbd02ee8ae5867f34
Sakthi Namasivayam <sakthiarun299@gmail.com>
2021-07-15 18:58:46 +0530

    Userdata JAVA

---
+++ LockWise/src/java/com/lockwise/services/userdata.java
@@ -0,0 +1,20 @@
+package com.lockwise.services;
+
+public class userdata {
+    public static  int id;
+    public static String username;
+    public static String email;
+    public static String password;
+    
+    public static String fullname;
+    public static String mobile;
+    public static String address;
+    public static String genre;
+    public static String gender;
+    
+    userdata(String u,String v)
+{
+username = u;
+email = v;
+}
+}
